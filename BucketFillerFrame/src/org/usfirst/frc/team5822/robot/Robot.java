f598af780909324dba2344d69d3ed9f63e49d288
sicp <sicp@10.76.201.182>
2017-02-04 14:27:03 -0600

    2/4
    final copy for today
    will need to be massively changed

--- BucketFillerFrame/src/org/usfirst/frc/team5822/robot/Robot.java
+++ BucketFillerFrame/src/org/usfirst/frc/team5822/robot/Robot.java
@@ -3,15 +3,21 @@ package org.usfirst.frc.team5822.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends IterativeRobot 
 {
 	RobotDrive wheelz;
+	AnalogSonar ultra;
 	
+	private final static int SONAR_PORT = 1;
+
 	@Override
 	public void robotInit() 
 	{
 		wheelz = new RobotDrive(0, 1, 2, 3);
+		ultra = new AnalogSonar(SONAR_PORT);
+		SmartDashboard.putNumber("Sonar Distance", ultra.RangeInInches());
 	}
 
 		@Override
@@ -47,6 +53,7 @@ public class Robot extends IterativeRobot
 		@Override
 		public void teleopPeriodic() 
 		{
+	
 			XboxFunctions.xboxButtons();
 			JoystickFunctions.joystickDrive(wheelz);
 			JoystickFunctions.visionLineUp(wheelz);
