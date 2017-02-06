1b19d34976454c8f171f2eaad4f3f98716be8fe9
sicp <sicp@10.76.201.182>
2017-02-06 15:50:39 -0600

    2/6/17 Patriots win the super bowl
    3:50

--- BucketFillerFrame/src/org/usfirst/frc/team5822/robot/OI.java
+++ BucketFillerFrame/src/org/usfirst/frc/team5822/robot/OI.java
@@ -1,6 +1,22 @@
 package org.usfirst.frc.team5822.robot;
 
+import org.usfirst.frc.team5822.robot.commands.Climb;
+import org.usfirst.frc.team5822.robot.commands.Shoot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
 public class OI 
 {
+	Joystick xboxCtr = new Joystick(1);
+	Button buttonX = new JoystickButton(xboxCtr, 3);
+	Joystick joystick = new Joystick(0);
+	Button button11 = new JoystickButton(joystick, 11);
 
+	public OI()
+	{
+		buttonX.whenPressed(new Shoot());
+		button11.whenPressed(new Climb());
+	}
 }
