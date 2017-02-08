0d3db6cb1e3766e113a6534ec420a6a90db8e243
sicp <sicp@10.76.105.150>
2017-02-08 16:45:55 -0600

    2/8/2017 Buttons Created. bela

--- BucketFillerFrame/src/org/usfirst/frc/team5822/robot/OI.java
+++ BucketFillerFrame/src/org/usfirst/frc/team5822/robot/OI.java
@@ -1,6 +1,11 @@
 package org.usfirst.frc.team5822.robot;
 
 import org.usfirst.frc.team5822.robot.commands.Climb;
+import org.usfirst.frc.team5822.robot.commands.GearLineUp;
+import org.usfirst.frc.team5822.robot.commands.GearVisionEnable;
+import org.usfirst.frc.team5822.robot.commands.HighGoalAngle;
+import org.usfirst.frc.team5822.robot.commands.HighGoalDistance;
+import org.usfirst.frc.team5822.robot.commands.HighGoalVisionEnable;
 import org.usfirst.frc.team5822.robot.commands.Shoot;
 
 import edu.wpi.first.wpilibj.Joystick;
@@ -10,13 +15,25 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
 public class OI 
 {
 	Joystick xboxCtr = new Joystick(1);
-	Button buttonX = new JoystickButton(xboxCtr, 3);
 	Joystick joystick = new Joystick(0);
+	Button button7 = new JoystickButton(joystick, 7);
+	Button button8 = new JoystickButton(joystick, 8);
+	Button button9 = new JoystickButton(joystick, 9);	
+	Button button10 = new JoystickButton(joystick, 10);
 	Button button11 = new JoystickButton(joystick, 11);
+	Button buttonY = new JoystickButton(xboxCtr, 4 );
+	Button buttonX = new JoystickButton(xboxCtr, 3);
 
 	public OI()
 	{
-		buttonX.whenPressed(new Shoot());
-		button11.whenPressed(new Climb());
+		buttonX.whileHeld(new Shoot());
+		buttonY.whenPressed(new Climb());
+		button7.whenPressed(new GearLineUp());
+		button8.whenPressed(new GearVisionEnable());
+		button9.whenPressed(new HighGoalAngle());
+		button10.whenPressed(new HighGoalDistance());
+		button11.whenPressed(new HighGoalVisionEnable());
+		
+		
 	}
 }
