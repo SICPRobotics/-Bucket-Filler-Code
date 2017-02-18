1ce42fe85c8f6fcd4a80b50b17ee0d773476104c
sicp <sicp@sicp-PC.ignatius.org>
2017-02-18 03:13:13 -0600

    Angela final code from 2/18 3:13

---
+++ FinalCodeForBRE/src/org/usfirst/frc/team5822/robot/SICPRobotDrive.java
@@ -0,0 +1,53 @@
+package org.usfirst.frc.team5822.robot;
+import com.ctre.CANTalon;
+
+import edu.wpi.first.wpilibj.MotorSafety;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.VictorSP;
+
+public class SICPRobotDrive extends RobotDrive implements MotorSafety
+{
+
+  /**
+   * Constructor for RobotDrive with 2 motors specified with channel numbers.
+   * Set up parameters for a two wheel drive system where the left and right
+   * motor pwm channels are specified in the call. This call assumes Victors for
+   * controlling the motors.
+   *$
+   * @param leftMotorChannel The PWM channel number that drives the left motor.
+   * @param rightMotorChannel The PWM channel number that drives the right
+   *        motor.
+   */
+	
+	//uses the CANTalons 2.10.17
+	
+  public SICPRobotDrive(final int leftMotorChannel, final int rightMotorChannel) 
+  {
+	  super(new CANTalon (leftMotorChannel), new CANTalon (rightMotorChannel));
+  }
+
+  /**
+   * Constructor for RobotDrive with 4 motors specified with channel numbers.
+   * Set up parameters for a four wheel drive system where all four motor pwm
+   * channels are specified in the call. This call assumes VictorSPs for
+   * controlling the motors.
+   *$
+   * @param frontLeftMotor Front left motor channel number
+   * @param rearLeftMotor Rear Left motor channel number
+   * @param frontRightMotor Front right motor channel number
+   * @param rearRightMotor Rear Right motor channel number
+   */
+  
+  //this passes VictorSPs to the RobotDrive Class! Greta 1.28 
+  
+  public SICPRobotDrive(final int frontLeftMotor, final int rearLeftMotor, final int frontRightMotor,
+      final int rearRightMotor) {
+
+      super(new CANTalon (frontLeftMotor), new CANTalon (rearLeftMotor), new CANTalon (frontRightMotor), new CANTalon (rearRightMotor)); 
+   
+  }
+
+  
+
+
+}
